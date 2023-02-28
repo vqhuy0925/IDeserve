89639ee2579682e4137222f48ff1cc8b94353efd
huy.vuquoc <huy.vuquoc@axonactive.com>
2023-02-28 21:42:52 +0700

    Update

--- Algorithm/src/LowestCommonAncestor.java
+++ Algorithm/src/LowestCommonAncestor.java
@@ -1,6 +1,6 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        ArraySortBST solution = new ArraySortBST();
+public class LowestCommonAncestor {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
         solution.root = new TreeNode(1);
         solution.root.left = new TreeNode(2);
         solution.root.right = new TreeNode(3);
@@ -10,15 +10,11 @@ public class App {
         solution.root.left.right.right = new TreeNode(8);
 
         TreeNode result = solution.lowestCommonAncestor(4, 8);
-        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
-        TreeNode sort = solution.sortArrayToBST(arr, 1, 7);
-
         System.out.println(result.val);
-        solution.preOrder(sort);
-    }
+    }    
 }
 
-class ArraySortBST {
+class Solution {
     TreeNode root;
 
     TreeNode lowestCommonAncestor(int n1, int n2) {
@@ -42,27 +38,5 @@ class ArraySortBST {
         } else {
             return left;
         }
-    }
-
-    TreeNode sortArrayToBST(int arr[], int start, int end){
-        if(start > end){
-            return null;
-        }
-
-        int mid = (start + end ) / 2;
-        TreeNode node = new TreeNode(arr[mid]);
-
-        node.left = sortArrayToBST(arr, start, mid-1);
-        node.right = sortArrayToBST(arr, mid + 1, end);
-        return node;
-    }
-
-    void preOrder(TreeNode node) {
-        if (node == null) {
-            return;
-        }
-        System.out.print(node.val + " ");
-        preOrder(node.left);
-        preOrder(node.right);
-    }
+    }        
 }
